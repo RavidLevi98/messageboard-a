eb55d321156f9207206325353cb8c58b44cb7b5c
Ravid Levi <61316827+RavidLevi98@users.noreply.github.com>
2021-03-24 19:25:57 +0200

    last 24

--- Board.cpp
+++ Board.cpp
@@ -7,69 +7,80 @@ namespace ariel
 {
     void Board::post(unsigned int row, unsigned int column, Direction direction, std::string message)
     {
-    if(row<MinRow){MinRow=row;}
-    if(column<MinCloumn && row<=MinRow){
-    MinRow = row;
-    MinCloumn = column;
-    }
-    if(MaxRow<row)
-    {
-        MaxRow = row;
-    }
-    if(MaxRow<=row && MaxClomn<column)
-    {
-        MaxRow=row;
-        MaxClomn=column;
-    }
-             
-            for (size_t i = 0; i < message.size(); i++)
-            {
-                if(direction==Direction::Horizontal)
-                {
-                    DynamicBoard[row][column+i]=message.at(i);
-                }
-                else if(direction==Direction::Vertical)
-                {
-                     DynamicBoard[row+i][column]=message.at(i);
-                }
-            }
-            
-        
+        if (row < MinRow)
+        {
+            MinRow = row;
+        }
+        if (column < MinCloumn && row <= MinRow)
+        {
+            MinRow = row;
+            MinCloumn = column;
+        }
+        if (MaxRow < row)
+        {
+            MaxRow = row;
+        }
+        if (MaxRow <= row && MaxClomn < column)
+        {
+            MaxRow = row;
+            MaxClomn = column;
+        }
 
-     
+        for (size_t i = 0; i < message.size(); i++)
+        {
+            if (direction == Direction::Horizontal)
+            {
+                DynamicBoard[row][column + i] = message.at(i);
+            }
+            else if (direction == Direction::Vertical)
+            {
+                DynamicBoard[row + i][column] = message.at(i);
+            }
+        }
     }
 
     std::string Board::read(unsigned int row, unsigned int column, Direction direction, unsigned int length)
     {
         std::string read = "";
-       for (size_t i = 0; i < length; i++)
+        for (size_t i = 0; i < length; i++)
+        {
+            if (direction == Direction::Horizontal)
             {
-                if(direction==Direction::Horizontal)
+                char cell = DynamicBoard[row][column + i];
+                if (isalpha(cell))
                 {
-                    char cell = DynamicBoard[row][column+i];
-                        read+=cell;
-                   
+                    read += cell;
                 }
-                else if(direction==Direction::Vertical)
+                else
                 {
-                     char cell = DynamicBoard[row+i][column];
-                   read+=cell;
+                    read += "_";
                 }
             }
+            else if (direction == Direction::Vertical)
+            {
+                char cell = DynamicBoard[row + i][column];
+                if (isalpha(cell))
+                {
+                    read += cell;
+                }
+                else
+                {
+                    read += "_";
+                }
+            }
+        }
 
         return read;
     }
-       void Board::show() {
-for (const auto& p : DynamicBoard)
-{
-    for (const auto& q : p.second) {
-     std::cout << q.second;
-}
-     std::cout << std::endl;
-
-}
-
-    
-        
+    void Board::show()
+    {
+        for (const auto &p : DynamicBoard)
+        {
+            for (const auto &q : p.second)
+            {
+                std::cout << q.second;
+            }
+            std::cout << std::endl;
+        }
     }
 }
